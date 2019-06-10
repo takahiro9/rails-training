1c50da9f8db6b1878fc847cb535f8b30c5e4e3bf
yamamototakahiro <yamamoto.takahiro@yukashikado.co.jp>
2019-06-10 11:10:33 +0900

    socery 便利機能使ってテストできた。

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'GET /users/:id', type: :request do
 
 	context '全てのパラメータが揃っている場合' do
 		before do
+			user = User.create(email: "hoge@gmail.com", password: "testtest")
+			login_user(user, "testtest")
 		end
 		it '200 OKを返す' do
 			get '/users/1', headers: { 'ACCEPT' => 'application/json' }
 			expect(response).to have_http_status(:ok)
 		end
-		it '成功時のJSONレスポンスを返す'
-		it 'ユーザを登録する'
 	end
 end
