d5bc9d2ebfd0c35d0692b993c0d44398024e5c03
yamamototakahiro <yamamoto.takahiro@yukashikado.co.jp>
2019-06-11 09:52:41 +0900

    動的風に修正

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'GET /users/:id', type: :request do
 
 	context '全てのパラメータが揃っている場合' do
 		before do
-			user = User.create(email: "hoge@gmail.com", password: "testtest")
-			login_user(user, "testtest")
+			@user = User.create(email: "hoge@gmail.com", password: "testtest")
+			login_user(@user, "testtest")
 		end
 		it '200 OKを返す' do
-			get '/users/1', headers: { 'ACCEPT' => 'application/json' }
+			get "/users/#{@user.id}", headers: { 'ACCEPT' => 'application/json' }
 			expect(response).to have_http_status(:ok)
 		end
 	end
